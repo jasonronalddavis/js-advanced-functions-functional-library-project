de5dbe1a91a455a3b8c699c7f674684311cbd664
jasonronalddavis <jdavis@c.ringling.edu>
2021-08-31 01:21:55 -0500

    commit tree 6183eef125b1588b31b78bf4e1c7e4a20e09c936

--- index.js
+++ index.js
@@ -26,11 +26,84 @@ let myEach = (collection, iteratee)=> {
   }
 
 
-  const testArr = [1, 2, 3, 4];
-  const unmodifiedTestObj = { one: 1, two: 2, three: 3, four: 4 };
-  const testObj = Object.assign({}, unmodifiedTestObj);
+
+  let myReduce = function(array, callback, acc) {
+    let values = (array instanceof Array) ? array.slice() : Object.values(array)
+
+    if (!acc) {
+      acc = values[0]
+      values =values.slice(1)
+    }
+    for (let i = 0; i < values.length; i++){
+      acc = callback(acc, values[i], array)
+    }
+    return acc  
+  }   
 
 
- 
+let myFind = function(collection, predicate){
+    if (!(collection instanceof Array))
+    collection = Object.values(collection)
+
+    for (let idx = 0; idx < collection.length; idx++)
+      if (predicate(collection[idx])) return collection[idx]
+
+    return undefined
+  }
+
+  let myFilter = function(collection, predicate){
+    if (!(collection instanceof Array))
+      collection = Object.values(collection)
+
+    const newArr = []
+
+    for (let idx = 0; idx < collection.length; idx++)
+      if (predicate(collection[idx])) newArr.push(collection[idx])
+
+    return newArr 
+  }
+
+  let mySize = function(collection) {
+    return (collection instanceof Array) ? collection.length : Object.keys(collection).length
+  }
+
+
+  let myFirst = function(collection, stop=false){
+    return (stop) ? collection.slice(0, stop) : collection[0]
+  }
+
+let myLast = function(collection, start=false){
+    return (start) ? collection.slice(collection.length-start, collection.length) : collection[collection.length-1]
+  }
+
+let myCompact = function(collection){
+    const badBad = new Set([false, null, 0, "", undefined, NaN])
+    return collection.filter(el => !badBad.has(el))
+  }
+
+let myKeys = function(obj){
+    const keys = []
+    for (let key in obj){
+      keys.push(key)
+    }
+    return keys
+  }
+
+
+  let myValues = function(obj){
+    const values = []
+    for (let key in obj){
+      values.push(obj[key])
+    }
+    return values
+  }
+
+
+
+
+
+
+
+
   
   
